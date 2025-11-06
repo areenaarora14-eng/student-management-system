09153d3953f9f2bce2376022ffb94786d662282d
areena <areenaarora14@gmail.com>
2025-11-06 09:28:33 +0530

    Add files via upload

---
+++ student management system.java
@@ -0,0 +1,15 @@
+class Student
+{
+    int id;
+    string Name;
+    string course;
+    int marks;
+
+    Student(int id, String name, String course, int marks) 
+    {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+}
