fa579e4e9dfef1499f1b56a5a88b682a4e1e081d
areena <areenaarora14@gmail.com>
2025-11-06 09:20:28 +0530

    add files

---
+++ student_management_system2.java
@@ -0,0 +1,15 @@
+class Student
+{
+    int id;
+    String name;
+    String course;
+    int marks;
+
+    Student(int id, String name, String course, int marks) 
+    {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.marks = marks;
+    }
+}
